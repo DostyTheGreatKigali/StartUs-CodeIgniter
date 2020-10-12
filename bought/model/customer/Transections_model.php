501163fc9ed1bb8fa5f037725379393dd44186eb
DostyTheGreatKigali <abumucal89@gmail.com>
2020-10-12 20:47:03 +0000

    Tried and failed transactiona api

--- bought/model/customer/Transections_model.php
+++ bought/model/customer/Transections_model.php
@@ -1,38 +1,38 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
-class Transections_model extends CI_Model {
+class Transections_model extends CI_Model
+{
 
 
 	public function save_transections($data)
 	{
-		$this->db->insert('transections',$data);
+		$this->db->insert('transections', $data);
 	}
 
-	
+
 	public function all_transection()
 	{
 		return $this->db->select('*')
-		->from('transections')
-		->where('user_id',$this->session->userdata('user_id'))
-		->get()
-		->result();
+			->from('transections')
+			->where('user_id', $this->session->userdata('user_id'))
+			->get()
+			->result();
 	}
 
-	public function get_cata_wais_transections($user_id="")
+	public function get_cata_wais_transections($user_id = "")
 	{
-		if ($user_id!="") {
+		if ($user_id != "") {
 			$user_id = $user_id;
-		}
-		else{
+		} else {
 			$user_id = $this->session->userdata('user_id');
 		}
 
 		$data = $this->db->select('*')
-		->from('transections')
-		->where('user_id', $user_id)
-		->get()
-		->result();
-		
+			->from('transections')
+			->where('user_id', $user_id)
+			->get()
+			->result();
+
 		$dep = 0;
 		$dep_f = 0;
 		$w_f = 0;
@@ -45,9 +45,9 @@ class Transections_model extends CI_Model {
 
 		foreach ($data as $value) {
 
-			if(@$value->transection_category=='deposit'){
+			if (@$value->transection_category == 'deposit') {
 
-				$deposit = $this->getFees('deposit',$value->releted_id);
+				$deposit = $this->getFees('deposit', $value->releted_id);
 				$dep_f = $dep_f + $deposit->fees;
 				$individule['d_fees'] = $dep_f;
 
@@ -55,95 +55,90 @@ class Transections_model extends CI_Model {
 				$individule['deposit'] = $dep;
 			}
 
-			if(@$value->transection_category=='withdraw'){
+			if (@$value->transection_category == 'withdraw') {
 
-				$withdraw = $this->getFees('withdraw',$value->releted_id);
+				$withdraw = $this->getFees('withdraw', $value->releted_id);
 				$w_f = $w_f + $withdraw->fees;
 				$individule['w_fees'] = $w_f;
 
-				$we = $we+$value->amount;
+				$we = $we + $value->amount;
 				$individule['withdraw'] = $we;
-
 			}
 
-			if(@$value->transection_category=='transfer'){
+			if (@$value->transection_category == 'transfer') {
 
-				$transfer = $this->getFees('transfer',$value->releted_id);
+				$transfer = $this->getFees('transfer', $value->releted_id);
 				$t_f = $t_f + $transfer->fees;
 				$individule['t_fees'] = $t_f;
 
-				$tras = $tras+$value->amount;
+				$tras = $tras + $value->amount;
 				$individule['transfar'] = $tras;
 			}
 
-			if(@$value->transection_category=='investment'){
-				$invest = $invest+$value->amount;
+			if (@$value->transection_category == 'investment') {
+				$invest = $invest + $value->amount;
 				$individule['investment'] = $invest;
 			}
 
-			if(@$value->transection_category=='reciver'){
-				$reciver = $reciver+$value->amount;
+			if (@$value->transection_category == 'reciver') {
+				$reciver = $reciver + $value->amount;
 				$individule['reciver'] = $reciver;
 			}
 		}
 
-			// My Payout
-			$my_payout = $this->db->select("sum(amount) as earns2")
-				->from('earnings')
-				->where('user_id',$this->session->userdata('user_id'))
-				->where('earning_type','type2')
-				->get()
-				->row();
-			$individule['my_payout'] = $my_payout->earns2;
+		// My Payout
+		$my_payout = $this->db->select("sum(amount) as earns2")
+			->from('earnings')
+			->where('user_id', $this->session->userdata('user_id'))
+			->where('earning_type', 'type2')
+			->get()
+			->row();
+		$individule['my_payout'] = $my_payout->earns2;
 
-			//Package Commission
-			$commission = $this->db->select("sum(amount) as earns1")
-				->from('earnings')
-				->where('user_id',$this->session->userdata('user_id'))
-				->where('earning_type','type1')
-				->get()
-				->row();
-			$individule['commission'] = $commission->earns1;
+		//Package Commission
+		$commission = $this->db->select("sum(amount) as earns1")
+			->from('earnings')
+			->where('user_id', $this->session->userdata('user_id'))
+			->where('earning_type', 'type1')
+			->get()
+			->row();
+		$individule['commission'] = $commission->earns1;
 
-			//team bonus
-			$bonus = $this->db->select("sum(bonus) as bonuss")
-				->from('user_level')
-				->where('user_id',$this->session->userdata('user_id'))
-				->get()
-				->row();
+		//team bonus
+		$bonus = $this->db->select("sum(bonus) as bonuss")
+			->from('user_level')
+			->where('user_id', $this->session->userdata('user_id'))
+			->get()
+			->row();
 
-			$individule['bonuss'] = $bonus->bonuss;
+		$individule['bonuss'] = $bonus->bonuss;
 
 
-			// total earning
-			$total_earn =  $individule['my_payout']+$individule['commission']+$individule['bonuss'];
-			$individule['earn'] = $total_earn;
-			#-----------------------
-			//TOTAL FEES
-			$total_fees = (@$individule['d_fees']+@$individule['w_fees']+@$individule['t_fees']);
-			#-----------------------
+		// total earning
+		$total_earn =  $individule['my_payout'] + $individule['commission'] + $individule['bonuss'];
+		$individule['earn'] = $total_earn;
+		#-----------------------
+		//TOTAL FEES
+		$total_fees = (@$individule['d_fees'] + @$individule['w_fees'] + @$individule['t_fees']);
+		#-----------------------
 
-			#---------------------------
-			# TOTAL GRAND BALENCE
-			$individule['balance'] = (@$individule['deposit']+@$individule['reciver']+@$total_earn)-(@$individule['withdraw']+@$individule['investment']+@$individule['transfar']+@$total_fees);
-			#----------------------------
-			return $individule;
-
+		#---------------------------
+		# TOTAL GRAND BALENCE
+		$individule['balance'] = (@$individule['deposit'] + @$individule['reciver'] + @$total_earn) - (@$individule['withdraw'] + @$individule['investment'] + @$individule['transfar'] + @$total_fees);
+		#----------------------------
+		return $individule;
 	}
 
 
 
 
 
-	public function getFees($table,$id)
+	public function getFees($table, $id)
 	{
 		return $this->db->select('*')
-		->from($table)
-		->where($table.'_id',$id)
-		->get()
-		->row();
+			->from($table)
+			->where($table . '_id', $id)
+			->get()
+			->row();
 	}
-
-
 }
- 
--- startus/controllers/api/Transaction.php
+++ startus/controllers/api/Transaction.php
@@ -3,17 +3,17 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 require APPPATH . 'libraries/REST_Controller.php';
 
-class Transection extends CI_Controller
+class Transaction extends REST_Controller
 {
     public function __construct()
     {
         parent::__construct();
 
-        if (!$this->session->userdata('isLogIn'))
-            redirect('login');
+        // if (!$this->session->userdata('isLogIn'))
+        //     redirect('login');
 
-        if (!$this->session->userdata('user_id'))
-            redirect('login');
+        // if (!$this->session->userdata('user_id'))
+        //     redirect('login');
 
         $this->load->model(array(
             'customer/transections_model',
@@ -21,19 +21,23 @@ class Transection extends CI_Controller
         ));
     }
 
-    public function index()
+
+    public function index_get()
     {
+        $data = $this->response(['user_id' => $_REQUEST['user_id'], 'api_token' => $_REQUEST['api_token']], REST_Controller::HTTP_OK);
+        // var_dump($post_user_data); die;
 
         $data = $this->transections_model->get_cata_wais_transections();
         $data['transection'] = $this->transections_model->all_transection();
         $data['title']   = display('transection');
-        $data['content'] = $this->load->view('customer/pages/transection', $data, true);
-        $this->load->view('customer/layout/main_wrapper', $data);
+        // $data['content'] = $this->load->view('customer/pages/transection', $data, true);
+        // $this->load->view('customer/layout/main_wrapper', $data);
+        return $this->response(['success' => TRUE, 'transactionsInfo' => $data], REST_Controller::HTTP_OK);
+        // $this->response(['success' => FALSE, 'message' => 'Invalid token'], REST_Controller::HTTP_OK);
     }
 
     public function transection_details($id = NULL, $table = NULL)
     {
-
 
         $data['title']   = display('transection_details');
         $data['my_info'] = $this->Profile_model->my_info();
