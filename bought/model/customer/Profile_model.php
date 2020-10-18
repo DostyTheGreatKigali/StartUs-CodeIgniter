fa39ac3beb2de2c3ad945b53f346cc05f8e6950e
DostyTheGreatKigali <abumucal89@gmail.com>
2020-10-18 16:03:57 +0000

    Buy, Deposit and Sell models

--- bought/model/customer/Profile_model.php
+++ bought/model/customer/Profile_model.php
@@ -1,14 +1,13 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-class Profile_model extends CI_Model {
-	
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+class Profile_model extends CI_Model
+{
+
 	public function my_info()
 	{
-		$user_id =$this->session->userdata('user_id');
+		$user_id = $this->session->userdata('user_id');
 		return $this->db->select('*')->from('user_registration')
-		->where('user_id',$user_id)
-		->get()
-		->row();
+			->where('user_id', $user_id)
+			->get()
+			->row();
 	}
-
 }
- 
